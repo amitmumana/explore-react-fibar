bf2d5c0424a61d0d41f8d6799ad98cb528d80f83
amit_mumana <amit.mumana@codeflashinfotech.in>
2023-07-01 19:57:07 +0530

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { useRef } from "react"
-import { useFrame } from "@react-three/fiber"
+import { useThree,extend ,useFrame } from "@react-three/fiber"
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls"
+import CustomObject from "./components/CustomObject"
+
+
+extend({ OrbitControls })
 
 function App() {
-  useFrame(() => {
-    console.log("tick")
+
+  const {camera, gl} = useThree()
+  const cubeRef = useRef()
+  const groupRef = useRef()
+  
+  
+  useFrame((state, delta) => {
+    // const angle = state.clock.elapsedTime * 0.3  
+    //  state.camera.position.x = Math.sin(angle) * 7
+    //  state.camera.position.z = Math.cos(angle) * 7
+    //  state.camera.lookAt(0,0,0)
+
+    cubeRef.current.rotation.y += delta
+    //   groupRef.current.rotation.y += delta
   })
+
 
   return (
     <>
-      <mesh position-x={-2}>
-        <sphereGeometry args={[1.5]} />
-        <meshBasicMaterial color="orange" />
-      </mesh>
-      <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
-        <boxGeometry />
-        <meshBasicMaterial color="blue" />
-      </mesh>
+
+     <orbitControls args={[camera, gl.domElement]} />
+
+     <directionalLight position={[1,2,3 ]} intensity={1.5} />
+     <ambientLight intensity={0.3} />
+    <group ref={ groupRef } >
+       <mesh position-x={-2}>
+           <sphereGeometry args={[1.5]} />
+           <meshStandardMaterial color="orange" />
+         </mesh>
+   
+         <mesh ref={ cubeRef } rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+           <boxGeometry />
+           <meshStandardMaterial color="blue" />
+         </mesh>
+    </group>
+  
       <mesh position-y={-2} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry />
-        <meshBasicMaterial color="greenyellow" />
+        <meshStandardMaterial color="greenyellow" />
       </mesh>
+
+      <CustomObject/>
+
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import { Canvas } from "@react-three/fiber"
+import * as THREE from 'three'
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <Canvas>
+  <Canvas 
+     gl={{
+       antialias: false ,
+       toneMapping: THREE.ACESFilmicToneMapping,
+       outputEncoding: THREE.sRGBEncoding
+     }}
+  //  flat  // to diseble tone maping  
+  //  orthographic
+  camera={ {
+    // fav: 45,
+    // zoom:100,
+    near:0.1,
+    far:200,
+    position: [3,2,6]
+
+  } } >
     <App />
   </Canvas>
 )
