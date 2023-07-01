2adb73a27fbd001401fc352c4858fa10c3e198c5
amit_mumana <amit.mumana@codeflashinfotech.in>
2023-07-01 14:51:29 +0530

    initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react"
+import { useFrame } from "@react-three/fiber"
 
 function App() {
+  useFrame(() => {
+    console.log("tick")
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <mesh position-x={-2}>
+        <sphereGeometry args={[1.5]} />
+        <meshBasicMaterial color="orange" />
+      </mesh>
+      <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshBasicMaterial color="blue" />
+      </mesh>
+      <mesh position-y={-2} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color="greenyellow" />
+      </mesh>
+    </>
+  )
 }
 
-export default App;
+export default App
